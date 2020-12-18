2f9ef3d496953d13badcd185b178dfa10d8c7b13
EugeneAA <76208402+EugeneAA@users.noreply.github.com>
2020-12-18 16:09:11 +0500

    Add files via upload

--- firefox/remove.js
+++ firefox/remove.js
@@ -5,16 +5,12 @@ function remove() {
   let els = document.getElementsByTagName(tag_name);
   Array.prototype.forEach.call(els, function(el) {
     if (el.href.includes("company")) {
-      console.log(el.href);
       var art_el = el.closest(child_name);
       art_el.innerHTML = '';
       art_el.remove();
-    } else {
-      // console.log(el.href);
     }
   });
 }
-// remove("a", "article");
 
 window.addEventListener('load', function () {
   remove();
