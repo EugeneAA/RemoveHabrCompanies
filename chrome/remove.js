cdbd57f99a46f55b8602bfd03783b4cbd945795d
EugeneAA <76208402+EugeneAA@users.noreply.github.com>
2020-12-18 16:08:54 +0500

    Chrome

---
+++ chrome/remove.js
@@ -0,0 +1,21 @@
+
+function remove() {
+  tag_name = "a";
+  child_name = "li";
+  let els = document.getElementsByTagName(tag_name);
+  Array.prototype.forEach.call(els, function(el) {
+    if (el.href.includes("company")) {
+      var art_el = el.closest(child_name);
+      art_el.innerHTML = '';
+      art_el.remove();
+    }
+  });
+}
+
+window.addEventListener('load', function () {
+  remove();
+});
+if ("onhashchange" in window) {
+  remove();
+}
+window.onhashchange = remove;
