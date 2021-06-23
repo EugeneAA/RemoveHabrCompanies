72fcb317f107a58e320cc14bf119211c024bf203
eugene <ek993@mail.ru>
2021-06-23 14:03:01 +0500

    new design fix

--- firefox/remove.js
+++ firefox/remove.js
@@ -1,12 +1,10 @@
-
 function remove() {
   tag_name = "a";
-  child_name = "li";
   let els = document.getElementsByTagName(tag_name);
   Array.prototype.forEach.call(els, function(el) {
     if (el.href.includes("company")) {
-      var art_el = el.closest(child_name);
-      if(art_el != null) 
+      var art_el = el.parentElement.parentElement;
+      if(art_el != null)
       {
         art_el.innerHTML = '';
         art_el.remove();
@@ -21,4 +19,28 @@ window.addEventListener('load', function () {
 if ("onhashchange" in window) {
   remove();
 }
+
+// Выбираем целевой элемент
+var target = document.getElementById('app');
+
+// Конфигурация observer (за какими изменениями наблюдать)
+const config = {
+    attributes: true,
+    childList: true,
+    subtree: true
+};
+
+// Колбэк-функция при срабатывании мутации
+const callback = function(mutationsList, observer) {
+  remove();
+};
+
+// Создаём экземпляр наблюдателя с указанной функцией колбэка
+const observer = new MutationObserver(callback);
+
+// Начинаем наблюдение за настроенными изменениями целевого элемента
+observer.observe(target, config);
+
+
+
 window.onhashchange = remove;
